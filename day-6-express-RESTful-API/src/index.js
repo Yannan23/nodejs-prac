9060d86035bf6692014a2e24eb8ba1ea53a0668d
Yannan23 <yannansun0723@gmail.com>
2025-03-09 06:15:15 +1100

    day-6 changes

--- day-6-express-RESTful-API/src/index.js
+++ day-6-express-RESTful-API/src/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
-const movieRouter = require('./routes/movie.router')
 const corsMiddleware = require('./middleware/cors.middleware')
+const v1Router = require('./routes')
 const app = express()
 
 app.use(express.json())
 app.use(corsMiddleware)
 
-app.use('/v1/movies', movieRouter)
+app.use('/v1', v1Router)
 
 app.listen(3000, () => {
     console.log('Server is listening on port 3000');
