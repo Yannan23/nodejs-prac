c9b4a702c854a11b52addb60f7834229b97daf02
Yannan23 <yannansun0723@gmail.com>
2025-04-20 17:46:54 +1000

    rename directory name and install express

--- day-5-npm-express/index.js
+++ day-5-npm-express/index.js
@@ -1,44 +1,44 @@
 //quiz
 
-const express = require('express');
-const app = express();
-function m1(req, res, next) {
-    console.log('m1');
-    next();
-}
-function m2(req, res, next) {
-    console.log('m2');
-    next();
-}
-function m3(req, res, next) {
-    console.log('m3');
-    next();
-}
-function m4(req, res, next) {
-    console.log('m4');
-    next();
-}
-function m5(req, res, next) {
-    console.log('m5');
-    res.json({ msg: 'm5' });
-}
-function m6(error, req, res, next) {
-    console.log('m6');
-    res.json({ msg: 'm6' });
-}
+// const express = require('express');
+// const app = express();
+// function m1(req, res, next) {
+//     console.log('m1');
+//     next();
+// }
+// function m2(req, res, next) {
+//     console.log('m2');
+//     next();
+// }
+// function m3(req, res, next) {
+//     console.log('m3');
+//     next();
+// }
+// function m4(req, res, next) {
+//     console.log('m4');
+//     next();
+// }
+// function m5(req, res, next) {
+//     console.log('m5');
+//     res.json({ msg: 'm5' });
+// }
+// function m6(error, req, res, next) {
+//     console.log('m6');
+//     res.json({ msg: 'm6' });
+// }
 
 
 
-app.use(m1);
-app.use(m6);
-app.use((req, res) => { res.status(404).json({ error: "path not found" }) })
-app.use('/v1', m2);
-app.get('/v1/tasks', m3, m4);
-app.get('/v1/tasks/:id', m5, (req, res) => {
-    res.json(req.params);
-});
+// app.use(m1);
+// app.use(m6);
+// app.use((req, res) => { res.status(404).json({ error: "path not found" }) })
+// app.use('/v1', m2);
+// app.get('/v1/tasks', m3, m4);
+// app.get('/v1/tasks/:id', m5, (req, res) => {
+//     res.json(req.params);
+// });
 
-app.listen(3000, () => console.log('listen on 3000'));
+// app.listen(3000, () => console.log('listen on 3000'));
 // quiz (what's get logged in the terminal and
 //  what's the response?)
 // 1. GET /v1
@@ -47,3 +47,25 @@ app.listen(3000, () => console.log('listen on 3000'));
 // 4. GET /v1/tasks/2/description
 
 // how to solve 404 response (DEFAULT -> {error:"path not found"})
+
+// const express = require('express')
+// const cors = require('cors')
+// const app = express()
+
+// app.use(cors());
+
+// app.get('/api/data', (req, res) => {
+//     res.json({ message: 'cors is enabled' })
+// })
+
+// app.listen(3000, () => {
+//     console.log("app is running on port 3000");
+
+// })
+
+const http = require('http')
+const server = http.createServer((req, res) => {
+    res.end('..')
+})
+
+server.listen(3000)
