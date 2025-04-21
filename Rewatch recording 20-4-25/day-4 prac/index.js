e2bcf174c95bf3535323e71a7cac1c26d4c37bf1
Yannan23 <yannansun0723@gmail.com>
2025-04-22 09:18:31 +1000

    add restfull api

--- Rewatch recording 20-4-25/day-4 prac/index.js
+++ Rewatch recording 20-4-25/day-4 prac/index.js
@@ -19,13 +19,32 @@ const movies = [
 ];
 
 app.get('/v1/movies', (req, res) => {
-    res.status(200).json(movies)
+    const { keyword, sort, limit = 10, page = 1 } = req.query
+
+    const filteredMovie = [...movies]
+    if (keyword) {
+        filteredMovie = filteredMovie.filter(
+            m => m.title.toLowerCase().includes(keyword.toLowerCase()) ||
+                m.description.toLowerCase().includes(keyword.toLowerCase())
+        )
+    }
+    if (sort === 'rating') {
+        filteredMovie.sort((a, b) => a.averageRating - b.averageRating)
+    } else if (sort === -'rating') {
+        filteredMovie.sort((a, b) => b.averageRating - a.averageRating)
+    }
+
+    const startIndex = parseInt(page - 1) * parseInt(limit)
+    const endIndex = parseInt(limit) + startIndex
+    const pagedmovie = filteredMovie.slice(startIndex, endIndex)
+
+    res.status(200).json(pagedmovie)
 })
 
 app.post('/v1/movies', (req, res, next) => {
     const { title, description, types } = req.body
     if (!title || !description || !Array.isArray(types) || types.length === 0) {
-        res.status(400).json({
+        return res.status(400).json({
             msg: "Missing ...."
         })
     }
@@ -45,7 +64,7 @@ app.get('/v1/movies/:id', (req, res, next) => {
     // const movie = movies.find(movie => movie.id.toString() === id)
     const movie = movies.find(movie => movie.id === +id)  //parseInt(id)
     if (!movie) {
-        res.status(400).json({
+        return res.status(404).json({
             msg: "Movie not found"
         })
     }
@@ -56,7 +75,7 @@ app.put('/v1/movies/:id', (req, res, next) => {
     const { id } = req.params
     const movie = movies.find(movie => movie.id === +id)
     if (!movie) {
-        res.status(400).json({
+        return res.status(404).json({
             msg: "Movie not found"
         })
     }
@@ -69,13 +88,66 @@ app.put('/v1/movies/:id', (req, res, next) => {
     }
     if (types) {
         if (!Array.isArray(types) || types.length === 0) {
-            res.status(400).json({
+            return res.status(400).json({
                 message: "Types should be an array"
             })
         }
         movie.types = types
     }
     res.status(201).json(movie)
+})
+
+app.delete('/v1/movies/:id', (req, res, next) => {
+    const { id } = req.params
+    const movieIndex = movies.findIndex(m => m.id === +id)
+    if (movieIndex === -1) {
+        return res.status(404).json({
+            msg: "Movie not found"
+        })
+    }
+    movies.splice(movieIndex, 1)
+    res.sendStatus(204)
+})
+
+app.post('/v1/movies/:id/reviews', (req, res, next) => {
+    const { id } = req.params
+    const movie = movies.find(movie => movie.id === +id)  //parseInt(id)
+    if (!movie) {
+        return res.status(404).json({
+            msg: "Movie not found"
+        })
+    }
+
+    const reviews = movie.reviews
+    let reviewId = movie.reviews.length + 1
+
+    const { content, rating } = req.body
+    if (!content || !rating || rating < 1 || rating > 5) {
+        res.status(400).json({
+            message: "Field cannot be blank"
+        })
+    }
+    const review = {
+        id: reviewId++,
+        content,
+        rating
+    }
+    reviews.push(review)
+    movie.averageRating = +(movie.reviews.reduce((sum, current) => sum + current.rating, 0) / movie.reviews.length).toFixed(2)
+    res.status(201).json(reviews)
+})
+
+app.get('/v1/movies/:id/reviews', (req, res, next) => {
+    const { id } = req.params
+    const movie = movies.find(movie => movie.id === +id)  //parseInt(id)
+    if (!movie) {
+        return res.status(404).json({
+            msg: "Movie not found"
+        })
+    }
+
+    const reviews = movie.reviews
+    res.status(200).json(reviews)
 })
 
 app.listen(3000, () => {
